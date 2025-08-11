317534b3b5cea8ed9ae513dac64816abc35ea726
Julia Gastellu <juliacgastellu@gmail.com>
2025-08-11 11:52:34 -0300

    .

--- eth-betting-frontend/src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    // The URL of the Flask prediction API.
-    // Make sure the Flask server (app.py) is running and accessible at this address.
-    API_URL: 'http://127.0.0.1:5000'
-};
-
-export default config;
